454633c42112d16772cbe5fdcd652ba4ab91beec
Danielle Bergbower <dkw5011@gmail.com>
2019-04-16 02:54:50 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -23,7 +23,7 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
- if phone.scan()
+ if phone.scan(/(?<!\d)\d{10}(?!\d)/)
    return true
  else
    false
