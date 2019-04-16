c5caa6ede629246c1d7f5f7361a311c7ce415ff9
Danielle Bergbower <dkw5011@gmail.com>
2019-04-16 02:54:26 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -23,7 +23,7 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
- if phone.scan(/\b\S\w\d{0,9}$\b/)
+ if phone.scan()
    return true
  else
    false
